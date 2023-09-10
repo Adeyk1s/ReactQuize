d4bc0d9403d713cc875a5b389c321c8e9e8651cc
Adeyk1s <136002550+Adeyk1s@users.noreply.github.com>
2023-09-10 15:38:28 +0500

    Finished

---
+++ src/FinishScreen.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function FinishScreen({ points, maxPossiblePoints, dispatch }) {
+  const percentage = (points / maxPossiblePoints) * 100;
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {maxPossiblePoints} (
+        {Math.ceil(percentage)}%)
+      </p>
+
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quize
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
---
+++ src/StartScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function StartScreen({ numQuestion, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quize</h2>
+      <h3>{numQuestion} question to test your React mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let`s start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
